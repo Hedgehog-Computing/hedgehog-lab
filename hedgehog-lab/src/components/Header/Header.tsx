1d3c10187026f6358ec2cb66110d8cd6c2a98c8b
Yan Lee <agdholo@gmail.com>
2020-07-20 01:29:21 +0800

    chore(header): increase the margin for the logo

--- hedgehog-lab/src/components/Header/Header.tsx
+++ hedgehog-lab/src/components/Header/Header.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Button, Toolbar, Typography, IconButton } from '@material-ui/core';
 
 const Header: React.FC<{}> = () => {
   return (
     <div style={{ flexGrow: 1 }} className={'header'}>
       <AppBar position="static" elevation={0} color="default">
         <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <img src={process.env.PUBLIC_URL + "/cat.png"} style={{ height: '1.25rem' }} />
+          </IconButton>
+
           <Typography variant="h6" style={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-            <img src={process.env.PUBLIC_URL + "/cat.png"} style={{ height: '1.25rem' }}/>
             Hedgehog Lab
           </Typography>
 
           <Button
             color="inherit"
             style={{ textTransform: 'none', height: 36 }}
-            onClick={() => {window.open('https://github.com/lidangzzz/hedgehog-lab')}}
+            onClick={() => { window.open('https://github.com/lidangzzz/hedgehog-lab') }}
           >
             <img
               alt="GitHub stars"
