6a4c5fd074559fc6d70f57e761ecf962107ab76f
Dolphin Wood <dolphin.w.e@gmail.com>
2020-07-26 07:16:23 +0900

    refactor(tutorials): use tag functions to convert TeX inputs

--- packages/hedgehog-lab/src/tutorials.ts
+++ packages/hedgehog-lab/src/tutorials.ts
@@ -16,14 +16,14 @@ print("Creating a matrix from csv: \\n" + b);
 //4. Also there are some useful built-in functions to create different kinds of matrix
 print("Creating a 3-by-3 eye matrix:\\n" + eye(3));
 print("Creating a random 2-by-3 matrix:\\n" + random(2,3));
-print("Creating a matrix of ones:\\n" + ones(2)); 
-print("Creating a matrix of zeros:\\n" + zeros(2)); 
+print("Creating a matrix of ones:\\n" + ones(2));
+print("Creating a matrix of zeros:\\n" + zeros(2));
 
 //5. You can also create a sequence of number starting with 1 ending with 20 and increasing with step 3, then reshape it as a 3-by-3 matrix
 print("Matrix c is:\\n" + range(1,20,3).reshape(3,3));
 `;
 
-const operatorsSource = `// Operator overloads are available for Mat class. 
+const operatorsSource = `// Operator overloads are available for Mat class.
 // Right operator could be Mat object, 2D or 1D array or number
 
 let A = mat([[1,2],[3,4]]);
@@ -46,7 +46,7 @@ print("A/2=\\n" + (A/2));
 // For element-wise multiplication (the same as A.*B in Matlab), use operator '**' instead
 print("A**A=\\n" + (A**A))
 
-// and if the right operand of operator "**" is a number N, it will generate an element-wise 
+// and if the right operand of operator "**" is a number N, it will generate an element-wise
 // power of N to the left operand matrix (the same as A.^N in Matlab)
 print("A**2=\\n" + A**2);
 
@@ -83,8 +83,8 @@ toc();
 `;
 
 const buildInFunctionsSource = `/*
-There are many built-in functions which support matrix, including     
-sin, cos, abs, acos, acosh, sign, 
+There are many built-in functions which support matrix, including
+sin, cos, abs, acos, acosh, sign,
 sqrt, trunc, floor, ceil, exp, log,
 asin, asinh, atan, atanh, tan, tanh,
 pow, round
@@ -97,7 +97,7 @@ A.digits = 5;
 
 print(sin(A));
 print(log(A));  // log A with base e
-print(log(A,2));  // log A with base 2 
+print(log(A,2));  // log A with base 2
 
 //More functions are on the way...
 `;
@@ -112,7 +112,7 @@ formulaTex("A=LL^{T},")
 // let's  calculate the cholesky now
 let L = chol(A).L;
 
-// and keep 5 digits 
+// and keep 5 digits
 L.digits = 5
 tex("\\\\text{where A is a positive-definite and symmetric matrix.} \\\\\\\\ \\\\text{For example, we have } A = " + A.toTex() + "\\\\text{, and the decomposed matrix L is }" + L.toTex())
 
@@ -130,15 +130,15 @@ formulaTex\`
 \`
 `;
 
-const graphicsSource = `// generate 2D points as vectors of x and y 
+const graphicsSource = `// generate 2D points as vectors of x and y
 let x = range(-10,10,0.1);
 let y = sin(x) + random(1,x.cols);
 
-// plot x and y as scatter 
-plot2D(x.toArray(),y.toArray());  
+// plot x and y as scatter
+plot2D(x.toArray(),y.toArray());
 
 // ploy x and y as line
-plot2DLine(x.toArray(),y.toArray());   
+plot2DLine(x.toArray(),y.toArray());
 
 // generate 3D points as vectors of x, y and z
 let size = 30;
@@ -150,15 +150,15 @@ let z = x**2 + y**2;
 // plot x,y,z as scatter in 3D
 plot3D(x.toArray(),y.toArray(), z.toArray());
 
-// mesh of x,y,z 
+// mesh of x,y,z
 plot3DMesh(x.toArray(),y.toArray(),z.toArray());
 
-/* For more advanced features and different kinds of charts, 
+/* For more advanced features and different kinds of charts,
    please check the official website plotly.js https://plotly.com/javascript/
    and use built-in function draw(data, layout) instead.
 */
 
-/* For more advanced features and different kinds of charts, 
+/* For more advanced features and different kinds of charts,
    please check the official website plotly.js https://plotly.com/javascript/
    and use built-in function draw(data, layout) instead.
 */
@@ -172,7 +172,7 @@ draw([{x:x.toArray(), y: y.toArray(), z: z.toArray(), mode: 'markers',marker: {c
 
 // Example 2 of draw()
 
-// dataset group 1 
+// dataset group 1
 let x1 = random(1,20)*2 + 2;
 let y1 = random(1,20)*3 + 3;
 
@@ -184,8 +184,8 @@ let y2 = random(1,20)* 0.8 - 1;
 let x3 = random(1,30) * 1 - 1;
 let y3 = random(1,30) * 3 + 2;
 
-draw( 
-[{x:x1.toArray(), y:y1.toArray(), mode:"markers"}, {x:x2.toArray(), y:y2.toArray(), mode:"markers"}, {x:x3.toArray(), y:y3.toArray(), mode:"markers"}], 
+draw(
+[{x:x1.toArray(), y:y1.toArray(), mode:"markers"}, {x:x2.toArray(), y:y2.toArray(), mode:"markers"}, {x:x3.toArray(), y:y3.toArray(), mode:"markers"}],
 {shapes: [{type:'circle', xref:'x', yref:'y', x0 : x1.min(), y0:y1.min(), x1: x1.max(), y1: y1.max(), opacity: 0.2, fillcolor: "green"}, {type:'circle', xref:'x', yref:'y', x0 : x2.min(), y0:y2.min(), x1: x2.max(), y1: y2.max(), opacity: 0.2, fillcolor: "blue"}, {type:'circle', xref:'x', yref:'y', x0 : x3.min(), y0:y3.min(), x1: x3.max(), y1: y3.max(), opacity: 0.2, fillcolor: "red"}], title:"Example 2 of draw(): cluster"}
 );
 `;
@@ -195,27 +195,27 @@ let x = sym('x')
 //write expression
 let fx = ( x^2 ) + exp(x) + sin(x)
 
-//print f(x) 
-formulaTex(\`f(x) = \` + fx.toTex())
+//print f(x)
+formulaTex\`f(x) = \${fx}\`
 
 //print f'(x)
-formulaTex(\`f'(x) = \` + diff(fx,x).toTex())
+formulaTex\`f'(x) = \${diff(fx,x)}\`
 
 //print integrate of f(x)
-formulaTex(\`\\\\int{f(x)dx} = \` + integrate(fx,x).toTex())
+formulaTex\`\\int{f(x)dx} = \${integrate(fx,x)}\`
 
 
 //define g(x)
 let gx = fx*fx + 1/fx + cos(x)
 
 //print g(x)
-formulaTex(\`g(x) = \` + gx.toTex())
+formulaTex\`g(x) = \${gx}\`
 
 //print g'(x)
-formulaTex(\`g'(x) = \` + diff(gx,x).toTex())
+formulaTex\`g'(x) = \${diff(gx,x)}\`
 
 //print integrate of g(x)
-formulaTex(\`\\\\int{g(x)dx} = \` + integrate(gx,x).toTex())
+formulaTex\`\\int{g(x)dx} = \${integrate(gx,x)}\`
 
 //define another two symbols
 let w = sym('w');
@@ -225,25 +225,25 @@ let y = sym('y');
 let W = (x^w) + sin(w+y) + (y^-2) + 1/w + log(cos(x) + sin(x))
 
 // W
-formulaTex(\`W(x,y,w) = \` + W.toTex())
+formulaTex\`W(x,y,w) = \${W}\`
 
 // dW / dx
-formulaTex(\`\\\\frac{dW(x,y,w)}{dx} = \` + diff(W,x).toTex());
+formulaTex\`\\frac{dW(x,y,w)}{dx} = \${diff(W,x)}\`
 
 // dW / dy
-formulaTex(\`\\\\frac{dW(x,y,w)}{dy} = \` + diff(W,y).toTex());
+formulaTex\`\\frac{dW(x,y,w)}{dy} = \${diff(W,y)}\`
 
 // dW / dw
-formulaTex(\`\\\\frac{dW(x,y,w)}{dw} = \` + diff(W,w).toTex());
+formulaTex\`\\frac{dW(x,y,w)}{dw} = \${diff(W,w)}\`
 
 // integral W on x
-formulaTex(\`\\\\int{W(x,y,w)dx} = \` + integrate(W,x).toTex());
+formulaTex\`\\int{W(x,y,w)dx} = \${integrate(W,x)}\`
 `;
 
 const markdownSource = `markdown(\`
 # Hedgehog Lab Markdown Example
 
-## Plain Text 
+## Plain Text
 
 This is an example for **plain text** in [Hedgehog Lab](https://github.com/lidangzzz/hedgehog-lab)
 
@@ -266,13 +266,13 @@ print(matrixB);
 ## Feel free to arrange any TeX, plotting and Markdown blocks in Hedgehog Lab!
 \`)
 
-formulaTex(\`A = \` + range(1,26).reshape(5,5).toTex())
+formulaTex\`A = \${range(1,26).reshape(5,5)}\`
 
 let x = sym('x')
 let fx = sin(x) + cos(x) + 1/x;
 
-tex(\`\\\\text{Let's define a function in hedgehog lab as: }f(x) = \` + fx.toTex() + \` \\\\text{, and the derivative of the function is:}\`);
-formulaTex(\`f'(x) = \` + diff(fx,x).toTex());
+tex\`\\text{Let's define a function in hedgehog lab as: }f(x) = \${fx} \\text{, and the derivative of the function is:}\`
+formulaTex\`f'(x) = \${diff(fx,x)}\`
 
 let X = range(1,10,0.01);
 let Y = sin(X);
