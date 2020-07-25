beaac059397a35699007ef206c61598a27428c92
imtsuki <me@qjx.app>
2020-07-25 11:15:22 +0800

    update CI build script
    Signed-off-by: imtsuki <me@qjx.app>

--- packages/hedgehog-lab/src/typed.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.less' {
-  const content: { [key: string]: any };
-  export default content;
-}
-
-declare module '*.worker.js' {
-  const content: any;
-  export default content;
-}
