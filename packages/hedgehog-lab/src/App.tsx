4cc149d7efd4d355c3e60b9b99a09c202d6c655b
agdholo <agdholo@gmail.com>
2021-10-03 18:26:04 +0800

    chore(theme): clean code

--- packages/hedgehog-lab/src/App.tsx
+++ packages/hedgehog-lab/src/App.tsx
@@ -1,62 +1,8 @@
 import React from 'react';
-import {createTheme, StyledEngineProvider, Theme, ThemeProvider} from '@mui/material/styles';
+import {StyledEngineProvider, ThemeProvider} from '@mui/material/styles';
 import './App.css';
 import HedgehogLab from './HedgehogLab';
-
-
-declare module '@mui/styles/defaultTheme' {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface DefaultTheme extends Theme {
-    }
-}
-
-
-const defaultShadow = '0 2px 30px -1px rgba(85,85,85,.08),0 4px 30px 0 rgba(85,85,85,.06),0 1px 30px 0 rgba(85,85,85,.03)'
-
-const theme = createTheme({
-    palette: {
-        mode: 'light'
-    },
-    components: {
-        MuiAppBar: {
-            styleOverrides: {
-                root: {
-                    boxShadow: defaultShadow
-                }
-            }
-        },
-        MuiDrawer: {
-            styleOverrides: {
-                paper: {
-                    borderRight: "none",
-                    boxShadow: defaultShadow,
-                    marginTop: 57
-                }
-            }
-        },
-        MuiButton: {
-            styleOverrides: {
-                root: {
-                    borderRadius: '10px',
-                    textTransform: 'capitalize'
-                }
-            }
-        },
-        MuiDialog: {
-            styleOverrides: {
-                root: {
-                    backgroundColor: 'rgba(32, 38, 45, 0.2)',
-                    backdropFilter: 'blur(2px)',
-                    transition: 'opacity 120ms ease 0s',
-                    opacity: 1
-                },
-                paper: {
-                    boxShadow: defaultShadow
-                }
-            }
-        }
-    }
-});
+import theme from "./config/theme";
 
 const App = (): React.ReactElement => (
     <div className="App">
---
+++ packages/hedgehog-lab/src/config/theme.tsx
@@ -0,0 +1,50 @@
+import {createTheme} from "@mui/material/styles";
+
+const defaultShadow = '0 2px 30px -1px rgba(85,85,85,.08),0 4px 30px 0 rgba(85,85,85,.06),0 1px 30px 0 rgba(85,85,85,.03)'
+
+const theme = createTheme({
+    palette: {
+        mode: 'light'
+    },
+    components: {
+        MuiAppBar: {
+            styleOverrides: {
+                root: {
+                    boxShadow: defaultShadow
+                }
+            }
+        },
+        MuiDrawer: {
+            styleOverrides: {
+                paper: {
+                    borderRight: "none",
+                    boxShadow: defaultShadow,
+                    marginTop: 57
+                }
+            }
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '10px',
+                    textTransform: 'capitalize'
+                }
+            }
+        },
+        MuiDialog: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: 'rgba(32, 38, 45, 0.2)',
+                    backdropFilter: 'blur(2px)',
+                    transition: 'opacity 120ms ease 0s',
+                    opacity: 1
+                },
+                paper: {
+                    boxShadow: defaultShadow
+                }
+            }
+        }
+    }
+});
+
+export default theme
