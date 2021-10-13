49c7f2747fc1580efd532b4aa3a41fc2e306645d
agdholo <agdholo@gmail.com>
2021-10-14 01:42:51 +0800

    feat(theme): create new theme

--- packages/hedgehog-lab/src/App.tsx
+++ packages/hedgehog-lab/src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {StyledEngineProvider, ThemeProvider} from '@mui/material/styles';
 import './App.css';
 import HedgehogLab from './HedgehogLab';
-import theme from "./config/theme";
 import {SnackbarProvider} from "notistack";
+import {labTheme} from "./config/labTheme";
 
 const App = (): React.ReactElement => (
     <div className="App">
         <SnackbarProvider maxSnack={3}>
             <StyledEngineProvider injectFirst>
-                <ThemeProvider theme={theme}>
+                <ThemeProvider theme={labTheme}>
                     <HedgehogLab/>
                 </ThemeProvider>
             </StyledEngineProvider>
