f66590cb33194f8ee2bc1ce665e9c8ab5f9b4182
agdholo <agdholo@gmail.com>
2021-11-07 19:11:07 +0800

    fix: memory leak

--- packages/hedgehog-lab/src/compiler.ts
+++ packages/hedgehog-lab/src/compiler.ts
@@ -1,60 +1,63 @@
 import CompilerWorker from "./compiler.worker?worker";
 import OutputWorker from "./output.worker?worker";
-import { OutputItem, isTextItem } from "hedgehog-core-js";
+import {isTextItem, OutputItem} from "hedgehog-core-js";
 import * as Comlink from "comlink";
+
 console.log(
-  import("hedgehog-core-js").then((e) => {
-    console.log(e.isDrawingItem);
-  })
+    import("hedgehog-core-js").then((e) => {
+        console.log(e.isDrawingItem);
+    })
 );
+
 export interface OutputResult {
-  outputItem: OutputItem[];
-  outputString: string;
+    outputItem: OutputItem[];
+    outputString: string;
 }
 
 type CancelablePromise<T> = Promise<T> & {
-  cancel: () => any;
+    cancel: () => any;
 };
 
 export const compiler = (...param: readonly [string, string]) => {
-  const compilerWorker = new CompilerWorker();
-  const outputWorker = new OutputWorker();
+    const compilerWorker = new CompilerWorker()
+    const outputWorker = new OutputWorker()
 
-  const compile = Comlink.wrap<{
-    compile: (data: string) => Promise<string>;
-  }>(compilerWorker).compile;
+    const compile = Comlink.wrap<{
+        compile: (data: string) => Promise<string>
+    }>(compilerWorker).compile
 
-  const output = Comlink.wrap<{
-    output: (data: string) => Promise<any[]>;
-  }>(outputWorker).output;
+    const output = Comlink.wrap<{
+        output: (data: string) => Promise<any[]>
+    }>(outputWorker).output
 
-  const run = async () => {
-    const code = await compile(param[1]);
-    const result = await output(code);
-    let outputString = "";
-    const outputItem = result;
-    outputItem.forEach((element: OutputItem) => {
-      if (isTextItem(element)) {
-        outputString += element.text + "\n";
-      }
-    });
-    return {
-      outputString,
-      outputItem,
-    };
-  };
+    const cleanup = () => {
+        compilerWorker.terminate()
+        outputWorker.terminate()
 
-  const cancel = () => {
-    compilerWorker.terminate();
-    outputWorker.terminate();
+        // unlisten worker
+        compile[Comlink.releaseProxy]()
+        output[Comlink.releaseProxy]()
+    }
 
-    // unlisten worker
-    compile[Comlink.releaseProxy]();
-    output[Comlink.releaseProxy]();
-  };
+    const run = async () => {
+        const code = await compile(param[1])
+        const result = await output(code)
+        let outputString = ""
+        const outputItem = result
+        outputItem.forEach((element: OutputItem) => {
+            if (isTextItem(element)) {
+                outputString += element.text + "\n"
+            }
+        })
+        cleanup()
+        return {
+            outputString,
+            outputItem,
+        }
+    }
 
-  const result = run();
-  // provide the cancel function for react-query
-  (result as CancelablePromise<OutputResult>).cancel = cancel;
-  return result;
-};
+    const result = run()
+        // provide the cancel function for react-query
+    ;(result as CancelablePromise<OutputResult>).cancel = cleanup
+    return result
+}
