a2e949f6e093ef3b254a8582cad30d3f0d83e5be
agdholo <agdholo@gmail.com>
2021-11-09 02:05:12 +0800

    chore(AuthSign): add username yup schema

--- packages/hedgehog-lab/src/components/User/Auth/AuthYupSchema.ts
+++ packages/hedgehog-lab/src/components/User/Auth/AuthYupSchema.ts
@@ -1,6 +1,7 @@
 import * as yup from "yup";
 
 export const AuthYupSchema = yup.object({
+    username: yup.string().required(),
     email: yup.string().email().required(),
     password: yup.string().min(6).required()
 }).required();
