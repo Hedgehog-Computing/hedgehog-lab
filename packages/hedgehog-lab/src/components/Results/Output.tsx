a0e15f9f0ec10fbb027427fee7ec0b1c031163e1
odysa <piensengxv@gmail.com>
2021-12-17 07:11:45 -0600

    feat: syntax highlight in markdown (#92)
    * feat: syntax highlight in markdown
    
    * chore(Theme): optimized markdown output in dark mode
    
    * chore(Theme): using dark & light markdown theme
    
    Co-authored-by: agdholo <agdholo@gmail.com>

--- packages/hedgehog-lab/src/components/Results/Output.tsx
+++ packages/hedgehog-lab/src/components/Results/Output.tsx
@@ -5,8 +5,17 @@ import MathJax from 'react-mathjax';
 import Markdown from 'react-markdown';
 import TableComponent from './OutputItemCompoments/TableComponent';
 import {isDrawingItem, isFormulaItem, isMarkdownItem, isTableItem, isTeXItem, OutputItem} from '@hedgehog/core';
+import type {CodeComponent} from 'react-markdown/src/ast-to-react'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {useRecoilValue} from "recoil";
+import {themeModState} from "../../themes/RThemeStates";
+import {atomOneDark, atomOneLight} from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+type CodeProps = Parameters<CodeComponent>[0]
 
 const Output = ({outputItemList}: { outputItemList: OutputItem[] }): React.ReactElement => {
+    const themeMode = useRecoilValue(themeModState)
+
     const items = outputItemList.map((item) => {
         if (isDrawingItem(item)) {
             return <Plot data={item.data} layout={item.layout}/>;
@@ -27,7 +36,23 @@ const Output = ({outputItemList}: { outputItemList: OutputItem[] }): React.React
                 </MathJax.Provider>
             );
         } else if (isMarkdownItem(item)) {
-            return <Markdown source={item.text}/>;
+            return <Markdown components={{
+                code({node, inline, className, children, ...props}: CodeProps): React.ReactElement {
+                    const match = /language-(\w+)/.exec(className || '')
+                    return !inline && match ? (
+                        <SyntaxHighlighter style={themeMode === 'light' ? atomOneLight : atomOneDark}
+                                           language={match[1]}
+                                           customStyle={{backgroundColor: 'transparent'}}
+                                           PreTag="div" {...props as any} >
+                            {String(children).replace(/\n$/, '')}
+                        </SyntaxHighlighter>
+                    ) : (
+                        <code className={className} {...props}>
+                            {children}
+                        </code>
+                    )
+                }
+            }}>{item.text}</Markdown>;
         } else if (isTableItem(item)) {
             return <TableComponent tableItem={item}/>;
         } else {
