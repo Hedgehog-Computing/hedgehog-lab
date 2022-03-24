5f8c4ac75e961cd5b224fce5ba572b565221a181
AGDholo <agdholo@gmail.com>
2022-03-24 18:54:42 +0800

    chore: add auth select button

--- packages/hedgehog-lab/src/pages/Snippets/Snippets.tsx
+++ packages/hedgehog-lab/src/pages/Snippets/Snippets.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import {Box, Button, CardActionArea, Chip, Divider, Link, Pagination, Paper, Typography} from "@mui/material";
-import {CopyBlock, dracula} from "react-code-blocks";
-import {StarBorderOutlined} from "@mui/icons-material";
-import {Link as RouterLink} from 'react-router-dom'
+import {
+  Box,
+  Button,
+  CardActionArea,
+  Chip,
+  Divider,
+  FormControl,
+  InputLabel,
+  Link,
+  MenuItem,
+  Pagination,
+  Paper,
+  Select,
+  Typography,
+} from "@mui/material";
+import { CopyBlock, dracula } from "react-code-blocks";
+import { StarBorderOutlined } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router-dom";
 import RenameDialog from "../../components/Snippet/Rename/RenameDialog";
 import DeletePopup from "../../components/Snippet/Delete/DeletePopup";
 import SharePopup from "../../components/Snippet/Share/SharePopup";
@@ -19,73 +33,102 @@ const printCode = `
     "builds": [
     
     
-`
+`;
 
 const Snippets = (): React.ReactElement => {
+  return (
+    <>
+      {Array.from(Array(3).keys()).map((item, index) => {
+        return (
+          <Box key={index}>
+            <Box display={"flex"} justifyContent={"space-between"}>
+              <Box alignItems={"center"} display={"flex"}>
+                <Link
+                  component={RouterLink}
+                  variant={"body1"}
+                  underline={"hover"}
+                  to={"/hhlab"}
+                >
+                  hhlab
+                </Link>
 
-    return (
-        <>
-            {Array.from(Array(3).keys()).map((item, index) => {
-                return (
-                    <Box key={index}>
-                        <Box display={'flex'} justifyContent={'space-between'}>
-                            <Box alignItems={"center"} display={"flex"}>
-                                <Link component={RouterLink} variant={'body1'} underline={"hover"} to={'/hhlab'}>
-                                    hhlab
-                                </Link>
+                <span style={{ margin: " 0 2px" }}>/</span>
 
-                                <span style={{margin: ' 0 2px'}}>/</span>
+                <Link
+                  component={RouterLink}
+                  variant={"body1"}
+                  underline={"hover"}
+                  to={"/hhlab/simple"}
+                >
+                  simple
+                </Link>
 
-                                <Link component={RouterLink} variant={'body1'} underline={"hover"} to={'/hhlab/simple'}>
-                                    simple
-                                </Link>
+                <Select
+                  fullWidth
+                  value={"Public"}
+                  size="small"
+                  sx={{
+                    height: 24,
+                    fontSize: "0.8125rem",
+                    ml: 1,
+                  }}
+                >
+                  <MenuItem dense value={"Public"}>
+                    Public
+                  </MenuItem>
+                  <MenuItem dense value={"private"}>
+                    Private
+                  </MenuItem>
+                </Select>
+              </Box>
 
-                                <Chip variant={'outlined'} label={'Secret'} size={'small'} sx={{ml: '10px'}}/>
-                            </Box>
+              <Box>
+                <Button color={"inherit"} startIcon={<StarBorderOutlined />}>
+                  1 stars
+                </Button>
 
-                            <Box>
-                                <Button color={'inherit'}
-                                        startIcon={<StarBorderOutlined/>}>
-                                    1 stars
-                                </Button>
+                <RenameDialog />
 
-                                <RenameDialog/>
+                <SharePopup url={"https://exampleSnippet.com"} />
 
-                                <SharePopup url={'https://exampleSnippet.com'}/>
+                <DeletePopup />
+              </Box>
+            </Box>
 
-                                <DeletePopup/>
-                            </Box>
-                        </Box>
+            <Box>
+              <Typography variant={"body2"}>
+                Last active 11 months ago <br />
+                Visual Studio Code Settings Sync Gist
+              </Typography>
 
-                        <Box>
-                            <Typography variant={'body2'}>
-                                Last active 11 months ago <br/>
-                                Visual Studio Code Settings Sync Gist
-                            </Typography>
+              <Paper elevation={0} sx={{ mt: "10px" }}>
+                <CardActionArea component={RouterLink} to={"/hhlab/simple"}>
+                  <Box
+                    sx={{
+                      "& button": {
+                        display: "none",
+                      },
+                    }}
+                  >
+                    <CopyBlock
+                      showLineNumbers
+                      text={printCode}
+                      language={"javascript"}
+                      theme={dracula}
+                    />
+                  </Box>
+                </CardActionArea>
+              </Paper>
+            </Box>
 
-                            <Paper elevation={0} sx={{mt: '10px'}}>
-                                <CardActionArea component={RouterLink} to={'/hhlab/simple'}>
-                                    <Box sx={{
-                                        '& button': {
-                                            display: 'none'
-                                        }
-                                    }}>
-                                        <CopyBlock showLineNumbers text={printCode} language={'javascript'}
-                                                   theme={dracula}/>
-                                    </Box>
-                                </CardActionArea>
-                            </Paper>
-                        </Box>
+            <Divider sx={{ my: "20px" }} />
+          </Box>
+        );
+      })}
 
-                        <Divider sx={{my: '20px'}}/>
-                    </Box>
-                )
-            })
-            }
+      <Pagination count={10} />
+    </>
+  );
+};
 
-            <Pagination count={10}/>
-        </>
-    )
-}
-
-export default Snippets
+export default Snippets;
