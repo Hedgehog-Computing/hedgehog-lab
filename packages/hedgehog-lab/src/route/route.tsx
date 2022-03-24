cb328b1bb297833bc8cd9fee91cc5eded8373afa
AGDholo <agdholo@gmail.com>
2022-03-24 17:36:48 +0800

    chore: align the top button when hiding the side

--- packages/hedgehog-lab/src/route/route.tsx
+++ packages/hedgehog-lab/src/route/route.tsx
@@ -1,4 +1,4 @@
-import {RouteObject, useLocation, useRoutes} from "react-router-dom";
+import { RouteObject, useLocation, useRoutes } from "react-router-dom";
 import * as React from "react";
 import Account from "../pages/Settings/Account";
 import Layout from "../components/Layout/Layout";
@@ -8,76 +8,70 @@ import ContainerLayout from "../components/Layout/ContainerLayout";
 import Auth from "../pages/Auth/Auth";
 
 const router: RouteObject[] = [
-    {
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
         path: "/",
-        element: <Layout/>,
+        element: <Main />,
+      },
+      {
+        path: "/tutorial/:tutorialID",
+        element: <Main />,
+      },
+      {
+        path: "/snippets/new",
+        element: <Main />,
+      },
+      {
+        path: "/:userID",
+        element: <ContainerLayout />,
         children: [
-            {
-                path: '/',
-                element: <Main/>
-            },
-            {
-                path: '/tutorial/:tutorialID',
-                element: <Main/>
-            },
-            {
-                path: '/snippets/new',
-                element: <Main/>
-            },
-            {
-                path: '/:userID',
-                element: <ContainerLayout/>,
-                children: [
-                    {
-                        path: '',
-                        element: <Snippets/>
-                    },
-                    {
-                        path: '/:userID/starred',
-                        element: <Snippets/>
-                    },
-                ]
-            },
-            {
-                path: '/:userID/:snippetID',
-                element: <Main/>
-            },
-            {
-                path: '/settings',
-                element: <ContainerLayout/>,
-                children: [
-                    {
-                        path: 'account',
-                        element: <Account/>
-                    }
-                ]
-            },
-            {
-                path: '/auth',
-                element: <ContainerLayout/>,
-                children: [
-                    {
-                        path: '',
-                        element: <Auth/>
-                    },
-                ]
-            },
-        ]
-    },
+          {
+            path: "",
+            element: <Snippets />,
+          },
+          {
+            path: "/:userID/starred",
+            element: <Snippets />,
+          },
+        ],
+      },
+      {
+        path: "/:userID/:snippetID",
+        element: <Main />,
+      },
+      {
+        path: "/settings",
+        element: <ContainerLayout />,
+        children: [
+          {
+            path: "account",
+            element: <Account />,
+          },
+        ],
+      },
+      {
+        path: "/auth",
+        element: <ContainerLayout />,
+        children: [
+          {
+            path: "",
+            element: <Auth />,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
-
 export const RoutePage = (): React.ReactElement => {
-    // modal route
-    const location = useLocation();
-    const state = location.state as { backgroundLocation?: Location };
+  // modal route
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location };
 
-    const element = useRoutes(router, state?.backgroundLocation || location)
+  const element = useRoutes(router, state?.backgroundLocation || location);
 
-    return (
-        <>
-            {element}
-        </>
-    )
-
-}
+  return <>{element}</>;
+};
