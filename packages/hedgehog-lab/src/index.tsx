4c66bf8834313a0136cdedc8699664a90522c078
agdholo <agdholo@gmail.com>
2022-04-09 22:23:56 +0800

    feat: upgrade react to 18

--- packages/hedgehog-lab/src/index.tsx
+++ packages/hedgehog-lab/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+// @ts-ignore
+const root = createRoot(container);
 
+root.render(<App/>)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
