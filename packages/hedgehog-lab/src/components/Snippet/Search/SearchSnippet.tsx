a4de23343e07a082144062853c93b072603d5968
agdholo <agdholo@gmail.com>
2022-04-25 16:50:59 +0800

    chore: integration API for the search

--- packages/hedgehog-lab/src/components/Snippet/Search/SearchSnippet.tsx
+++ packages/hedgehog-lab/src/components/Snippet/Search/SearchSnippet.tsx
@@ -1,81 +1,78 @@
-import {
-  CodeOffOutlined,
-  CodeOutlined,
-  FavoriteBorderOutlined,
-  FavoriteOutlined,
-  StarBorderOutlined,
-} from "@mui/icons-material";
-import {
-  Box,
-  OutlinedInput,
-  Select,
-  MenuItem,
-  IconButton,
-  Tooltip,
-  Button,
-} from "@mui/material";
-import { grey } from "@mui/material/colors";
-import React, { useCallback, useState } from "react";
-import { useRecoilState } from "recoil";
-import { useAuth } from "../../../hooks/useAuth";
-import { showCodeBlockState } from "../../../states/RSnippetStates";
+import {CodeOffOutlined, CodeOutlined, FavoriteBorderOutlined,} from "@mui/icons-material";
+import {Box, IconButton, MenuItem, OutlinedInput, Select, SelectChangeEvent, Tooltip,} from "@mui/material";
+import React, {useCallback} from "react";
+import {useRecoilState} from "recoil";
+import {useAuth} from "../../../hooks/useAuth";
+import {searchState, showCodeBlockState} from "../../../states/RSnippetStates";
 
 const SearchSnippet = (): React.ReactElement => {
-  const [showCodeBlock, setShowCodeBlock] = useRecoilState(showCodeBlockState);
-  const { isMe } = useAuth();
+    const [showCodeBlock, setShowCodeBlock] = useRecoilState(showCodeBlockState);
+    const [search, setSearch] = useRecoilState(searchState);
+    const {isMe} = useAuth();
 
-  const handleShowCodeBlock = useCallback(() => {
-    setShowCodeBlock(!showCodeBlock);
-  }, [setShowCodeBlock, showCodeBlock]);
+    const handleShowCodeBlock = useCallback(() => {
+        setShowCodeBlock(!showCodeBlock);
+    }, [setShowCodeBlock, showCodeBlock]);
 
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        width: "100%",
-        alignItems: "center",
-      }}
-    >
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <OutlinedInput
-          size="small"
-          placeholder={`Search ${isMe ? "your" : "all"} snippets`}
-        />
-        <Select
-          value={"5"}
-          sx={{
-            height: 40,
-            fontSize: "0.8125rem",
-            ml: 1,
-          }}
+
+    const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch({...search, text: event.target.value});
+    }, [setSearch, search]);
+
+    const handleChangeSize = useCallback((event: SelectChangeEvent) => {
+        setSearch({...search, size: parseInt(event.target.value)});
+    }, [setSearch, search])
+    return (
+        <Box
+            sx={{
+                justifyContent: "space-between",
+                display: "flex",
+                width: "100%",
+                alignItems: "center",
+            }}
         >
-          <MenuItem dense value={"5"}>
-            5
-          </MenuItem>
-          <MenuItem dense value={"10"}>
-            10
-          </MenuItem>
-        </Select>
-      </Box>
+            <Box sx={{display: "flex", alignItems: "center"}}>
+                <OutlinedInput
+                    size="small"
+                    placeholder={`Search ${isMe ? "your" : "all"} snippets`}
+                    value={search.text}
+                    onChange={handleChange}
+                />
+                <Select
+                    onChange={handleChangeSize}
+                    value={String(search.size)}
+                    sx={{
+                        height: 40,
+                        fontSize: "0.8125rem",
+                        ml: 1,
+                    }}
+                >
+                    <MenuItem dense value={'5'}>
+                        5
+                    </MenuItem>
+                    <MenuItem dense value={'10'}>
+                        10
+                    </MenuItem>
+                </Select>
+            </Box>
 
-      <Box>
-        <Tooltip title={showCodeBlock ? "Hide code" : "Show code"} arrow>
-          <IconButton onClick={handleShowCodeBlock}>
-            {showCodeBlock ? <CodeOutlined /> : <CodeOffOutlined />}
-          </IconButton>
-        </Tooltip>
+            <Box>
+                <Tooltip title={showCodeBlock ? "Hide code" : "Show code"} arrow>
+                    <IconButton onClick={handleShowCodeBlock}>
+                        {showCodeBlock ? <CodeOutlined/> : <CodeOffOutlined/>}
+                    </IconButton>
+                </Tooltip>
 
-        {isMe && (
-          <Tooltip title={"Favorite"} arrow>
-            <IconButton>
-              <FavoriteBorderOutlined />
-            </IconButton>
-          </Tooltip>
-        )}
-      </Box>
-    </Box>
-  );
+                {isMe && (
+                    <Tooltip title={"Favorite"} arrow>
+                        <IconButton>
+                            <FavoriteBorderOutlined/>
+                        </IconButton>
+                    </Tooltip>
+                )}
+            </Box>
+        </Box>
+    );
 };
 
 export default SearchSnippet;
--- packages/hedgehog-lab/src/components/Snippet/Snippet.tsx
+++ packages/hedgehog-lab/src/components/Snippet/Snippet.tsx
@@ -1,13 +1,20 @@
 import {Box, Divider, Pagination} from "@mui/material";
-import React, {useState} from "react";
+import React from "react";
 import SearchSnippet from "./Search/SearchSnippet";
 import useSWR from "swr";
 import {fetcher} from "../../network/fetcher";
 import SnippetList from "./List/SnippetList";
+import {Skeleton} from "@mui/lab";
+import {useRecoilState} from "recoil";
+import {searchState} from "../../states/RSnippetStates";
 
 const Snippet = () => {
-    const [size, setSize] = useState(5)
-    const {data, error} = useSWR([`/aws-open-search?q=*:*&from=0&size=${size}`], fetcher);
+    const [search, setSearch] = useRecoilState(searchState)
+
+    const q = search.text ? search.text : '*:*'
+
+    const {data, error} = useSWR([`/aws-open-search?q=${q}&from=${search.from}&size=${search.size}`], fetcher);
+
 
     return (
         <>
@@ -19,9 +26,13 @@ const Snippet = () => {
                 <>
                     <SnippetList snippets={data['hits']}/>
                     <Box sx={{display: "flex", justifyContent: "center"}}>
-                        <Pagination count={data['total'] && Math.ceil(data['total']['value'] / size)}/>
+                        <Pagination count={data['total'] && Math.ceil(data['total']['value'] / search.size)}/>
                     </Box>
                 </>
+            )}
+
+            {!data && (
+                <Skeleton variant="rectangular" width="100%" height={200}/>
             )}
 
 
