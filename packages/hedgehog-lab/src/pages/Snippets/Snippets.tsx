e7be96a36d2f896896b7f99041953b2767162d88
Yan Lee <agdholo@gmail.com>
2022-04-29 23:09:36 +0800

    feat(snippet) fetch different user snippet (#176)
    * fix: fetch different user snippet
    
    * fix: type the correct variable
    
    * chore: add user meta

--- packages/hedgehog-lab/src/pages/Snippets/Snippets.tsx
+++ packages/hedgehog-lab/src/pages/Snippets/Snippets.tsx
@@ -1,11 +1,17 @@
 import {Box, Card, CardContent, Grid, Link, Typography} from "@mui/material";
 import React from "react";
 import Snippet from "../../components/Snippet/Snippet";
-import {Link as RouterLink} from "react-router-dom";
+import {Link as RouterLink, useMatch} from "react-router-dom";
 import {useAuth} from "../../hooks/useAuth";
+import {useRecoilValue} from "recoil";
+import {userMetaState} from "../../states/RSnippetStates";
 
 const Snippets = (): React.ReactElement => {
     const {auth} = useAuth()
+    const currentUser = useMatch('/u/:userId')
+    const currentUserLikes = useMatch('/u/:userId/likes')
+    const name = currentUser?.params.userId ?? currentUserLikes?.params.userId
+    const userMeta = useRecoilValue(userMetaState)
 
     return (
         <>
@@ -14,17 +20,17 @@ const Snippets = (): React.ReactElement => {
                     <Card elevation={0} variant="outlined">
                         <CardContent>
                             <Typography variant="h5" fontWeight={"bold"}>
-                                {auth.user.firstname}
+                                {name}
                             </Typography>
 
                             <Box sx={{mt: 1}}>
-                                <Link component={RouterLink} to={`/u/${auth.user.firstname}`}>
-                                    394 Snippets
+                                <Link component={RouterLink} to={`/u/${name}`}>
+                                    {userMeta.snippet.count} Snippets
                                 </Link>
                             </Box>
                             <Box>
-                                <Link component={RouterLink} to={`/u/${auth.user.firstname}/likes`}>
-                                    43 Liked Snippets
+                                <Link component={RouterLink} to={`/u/${name}/likes`}>
+                                    x Liked Snippets
                                 </Link>
                             </Box>
                         </CardContent>
