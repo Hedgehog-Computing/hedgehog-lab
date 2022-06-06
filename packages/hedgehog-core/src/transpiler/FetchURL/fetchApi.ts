6aea0850941caa2b231540f5cfd4b9fa051d0352
Blake Wang <blakexwang@gmail.com>
2022-06-06 15:50:50 -0400

    updated fetchApi

--- packages/hedgehog-core/src/transpiler/FetchURL/fetchApi.ts
+++ packages/hedgehog-core/src/transpiler/FetchURL/fetchApi.ts
@@ -1,5 +1,18 @@
 const fetchApi = (username: string, filename: string): Promise<string> => {
-  const api = 'https://api.hlab.app/snippets?user=' + username + '&title=' + filename;
+  const host = location.hostname;
+  let apiHost;
+  switch (host) {
+    case "preview.hlab.app":
+      apiHost = "https://api.preview.hlab.app";
+      break;
+    case "hlab.app":
+      apiHost = "https://api.hlab.app";
+      break;
+    default:
+      apiHost = "http://localhost:9000";
+  }
+  
+  const api = apiHost + '/snippets?user=' + username + '&title=' + filename;
   return fetch(api, { method: 'get' }).then(res => res.json()).then((data) => {
     const returnResponseStatusCode = data.statusCode;
     if (returnResponseStatusCode !== 200) {
