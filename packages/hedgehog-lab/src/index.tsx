48429b482274131f18a7cf4c796c734d572d45d0
Blake Wang <blakexwang@gmail.com>
2022-07-24 21:07:26 -0400

    added PWA support

--- packages/hedgehog-lab/src/index.tsx
+++ packages/hedgehog-lab/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {createRoot} from "react-dom/client";
 
 const container = document.getElementById('root');
@@ -12,4 +12,4 @@ root.render(<App/>)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorkerRegistration.register();
--- packages/hedgehog-lab/src/serviceWorkerRegistration.ts
+++ packages/hedgehog-lab/src/serviceWorkerRegistration.ts
@@ -104,7 +104,7 @@ function registerValidSW(swUrl: string, config?: Config) {
 function checkValidServiceWorker(swUrl: string, config?: Config) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' }
+    headers: { 'Service-Worker': 'script' },
   })
     .then((response) => {
       // Ensure service worker exists, and that we really are getting a JS file.
