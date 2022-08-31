e1a2f1696600c95adb5cd5089955aea8c59de300
立党 Lidang <wxr001@gmail.com>
2022-08-30 23:27:42 -0500

    add

--- packages/hedgehog-core/test/js-embedded-test/test.ts
+++ packages/hedgehog-core/test/js-embedded-test/test.ts
@@ -3,15 +3,18 @@ import { transpile, executeOutput } from '../../src/index';
 import { isEqual } from 'lodash';
 
 const testCode1 = `
-let a = [[1,2],[3,4]];
-let b = [[5,6],[7,8]];
-let c = a + b;
+let a = 1;
 *js-start
-
+let d = a + 1;
 *js-end
-print( c ===[ [ 6, 8 ], [ 10, 12 ] ] );
+print( d===2 );
 `;
 
 describe('The embedded-js test', async () => {
-
+  const transpileResult = await transpile(testCode1);
+  const executeResult = await executeOutput(transpileResult);
+  console.log(executeResult);
+  it('should pass', async () => {
+    //assert.equal(isEqual)
+  });
 });
