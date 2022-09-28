8e86e99df6faed34ae4525df3fdda5934a681eac
立党 Lidang <wxr001@gmail.com>
2022-09-28 03:41:18 -0500

    Update hc.ts

--- packages/hedgehog-cli/src/hc.ts
+++ packages/hedgehog-cli/src/hc.ts
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 import {executeOutput, transpile} from '@hedgehogcomputing/core';
+import { boolean } from 'yargs';
 
 const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
 const hhsFilePath = argv.f;
-const hhsUserAndScript = argv._;
+const hhsScriptPath = argv._;
 const hhOutputItems = argv.o;
 
+let executeFile = false;
+let executeScript = false;
 if (typeof hhsFilePath === 'string') {
   console.log(hhsFilePath)
 }
 
-if (typeof hhsUserAndScript === 'string') {
-  console.log(hhsUserAndScript)
+if (typeof hhsScriptPath === 'string') {
+  console.log(hhsScriptPath)
 }
 
 if (typeof hhOutputItems === 'string') {
