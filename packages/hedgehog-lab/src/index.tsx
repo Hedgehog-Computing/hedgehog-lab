2be7588c579510869a0195b3131d4ae93e407a34
agdholo <agdholo@gmail.com>
2022-10-12 20:48:04 +0800

    fix: ci build

--- packages/hedgehog-lab/src/index.tsx
+++ packages/hedgehog-lab/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {createRoot} from "react-dom/client";
 
 const container = document.getElementById('root');
