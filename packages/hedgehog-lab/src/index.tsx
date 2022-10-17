71b469c74b1dd86d37f147ad4d9bb91036ea38b1
agdholo <agdholo@gmail.com>
2022-10-17 08:50:46 +0800

    fix(lab): clean up the index.tsx

--- packages/hedgehog-lab/src/index.tsx
+++ packages/hedgehog-lab/src/index.tsx
@@ -8,7 +8,3 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(<App/>)
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorkerRegistration.register();
